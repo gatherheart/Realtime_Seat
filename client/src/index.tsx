52afb12d9c42a98190db396e7c5b77bf5bf169b2
junghyuk.park <junghyuk.park@navercorp.com>
2020-11-19 10:55:19 +0900

    [Client] ADD: set apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider as ReduxProvider } from 'react-redux'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './apolloClient'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { composeWithDevTools } from 'redux-devtools-extension' // debugger
 
@@ -13,10 +15,12 @@ const store = createStore(reducer, composeWithDevTools({})())
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <CssBaseline />
-      <App />
-    </ReduxProvider>
+    <ApolloProvider client={client}>
+      <ReduxProvider store={store}>
+        <CssBaseline />
+        <App />
+      </ReduxProvider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
