e4e4a1300d929fcb16c2991eb2fb6f757982f246
junghyuk.park <junghyuk.park@navercorp.com>
2020-12-02 15:50:07 +0900

    [Server] FEAT: add issuing token api
    add query `token` that issues a token for guest user

---
+++ server/src/api/user/user.resolvers.ts
@@ -0,0 +1,19 @@
+import { sign } from 'jsonwebtoken'
+
+let guestId = 0
+
+const resolvers = {
+  Query: {
+    token: () => {
+      const JWT_SECRET = process.env.JWT_SECRET ?? 'SECRET'
+      const payload = {
+        sub: 'realtime-seat guest token',
+        aud: 'guest' + guestId,
+      }
+      guestId += 1
+      return sign(payload, JWT_SECRET)
+    },
+  },
+}
+
+export default resolvers
