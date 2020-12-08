ee947d5097e826de04441a890978e561ec8e7a36
hyunu-kim <hyunu.kim@navercorp.com>
2020-12-08 14:44:09 +0900

    [Client] deleted unnecessary package modules

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,17 +4,34 @@ import { GraphQLServer } from 'graphql-yoga'
 import schema from './schema'
 import { connect } from './db/database'
 import * as logger from 'morgan'
+import * as cors from 'cors'
 import contextMiddleware from './contextMiddleware'
 
 config({ path: resolve(__dirname, '../.env') })
 const PORT = process.env.PORT || 4000
-
 const server = new GraphQLServer({
   schema,
   context: contextMiddleware,
 })
 const options = {
   port: PORT,
+  subscriptions: {
+    onConnect: (connectionParams, webSocket) => {
+      console.log('Websocket CONNECTED', connectionParams)
+      return {
+        hello: 'world',
+      }
+    },
+    onDisconnect: () => console.log('Websocket DISCONNECTED'),
+  },
+}
+
+//options for cors midddleware
+const corsOptions: cors.CorsOptions = {
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token'],
+  methods: 'GET,POST,OPTIONS',
+  origin: '*',
+  credentials: true,
 }
 
 // Connect to DB
@@ -22,3 +39,4 @@ connect()
 // Server Start
 void server.start(options, () => console.log(`Server is running on http://localhost:${PORT}`))
 server.express.use(logger('dev'))
+server.express.use(cors(corsOptions)) // CORS Middleware
